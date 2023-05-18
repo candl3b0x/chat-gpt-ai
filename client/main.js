85dcfedb5cb5b5f64e212941a8dcf222b44a52a7
Sudip <sudip.grg080@gmail.com>
2023-05-17 21:00:31 -0700

    container creation

--- client/main.js
+++ client/main.js
@@ -1,7 +1,6 @@
 import "./style.css";
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-   
-  </div>
-`;
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
